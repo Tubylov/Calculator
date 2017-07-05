f5c618301c9e53969182845efb719db9df8b11ec
Tubylov <ilya.tubylov@yandex.ru>
2017-07-05 10:13:01 +0400

    1

---
+++ WebCalc/Models/CalcModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace WebCalc.Models
+{
+    public class CalcModel
+    {
+        public CalcModel()
+        {
+            OperationList = new List<SelectListItem>();
+        }
+
+        public string Operation { get; set;}
+
+        public double? X { get; set; }
+
+        public double? Y { get; set; }
+
+        public double[] Arguments
+        {
+            get
+            {
+                return new[] { X ?? 0, Y ?? 0 };
+            }
+        }
+
+        public double? Result { get; set; }
+
+        public IEnumerable<SelectListItem> OperationList { get; set; }
+    }
+}
